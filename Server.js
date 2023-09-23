d4817bedcd5c2c133100d4c98b19f7d2ffd8c7ea
DareIsrael <dareisrael4@gmail.com>
2023-09-23 01:51:09 -0700

    new commit

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@
  const app = express();
  app.use(cors({
    credentials: true,
-   origin: "https://mern-stack-app-ex9h.onrender.com"
+   origin: "https://mernfrontend-nine.vercel.app/"
  }));
 
  app.use(express.json())
